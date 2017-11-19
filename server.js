4bc9beddc5f3c9bf9064fef4bbeec03791d0d022
Soo Jeong Lee <soo.leesj@gmail.com>
2017-11-19 01:53:12 -0800

    Update server.js
    changing the port number

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(session({secret:'SuperSecretPassword'}));
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 42185);
 app.use(express.static(__dirname + '/img'));
 
 app.get('/',function(req,res){
